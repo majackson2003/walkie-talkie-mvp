88b9f3660d0f5da4e3a52fd2ed003e52ee08264a
Mark Jackson <mjackson@bcssaints.org>
2026-02-06 04:53:48 -0500

    Use same-origin socket URL in production

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const SERVER_URL = import.meta.env.VITE_SERVER_URL ?? 'http://localhost:3001';
+const SERVER_URL =
+  import.meta.env.VITE_SERVER_URL ??
+  (import.meta.env.PROD ? window.location.origin : 'http://localhost:3001');
 
 export const socket = io(SERVER_URL, {
   transports: ['websocket'],
